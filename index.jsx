ef31c3bbacd51123112f62f6830a77039e7cdfae
ianagbip1oti <ianagbip1oti@gmail.com>
2017-10-29 03:34:52 +0000

    Added graphql call. 404 cos no server

--- index.jsx
+++ index.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+
 import { Container, Header, Menu, Segment } from "semantic-ui-react";
 
-import { SendMessage } from "./mat-che/components.jsx";
+import { SendMessageWithData } from "./mat-che/components.jsx";
+
+import { ApolloClient, ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient();
 
 const App = () => (
-  <div>
-    <Header dividing>Mat Che</Header>
-    <SendMessage />
-  </div>
+  <ApolloProvider client={client}>
+    <div>
+      <Header dividing>Mat Che</Header>
+      <SendMessageWithData />
+    </div>
+  </ApolloProvider>
 );
 
 render(<App />, document.getElementById("app"));
--- mat-che/components.jsx
+++ mat-che/components.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 
-import { Input } from "semantic-ui-react";
+import { gql, graphql } from "react-apollo";
 
-export class SendMessage extends React.Component {
-  render() {
-    return <Input label="Name" icon={{ name: "send", link: true }} fluid />;
+import { Icon, Input, Message } from "semantic-ui-react";
+
+const meQuery = gql`
+  query meQuery {
+    me {
+      name
+    }
   }
-}
+`;
+
+export const SendMessage = ({ data: { loading, error, me } }) => {
+  if (loading) {
+    return (
+      <Message info icon>
+        <Icon name="circle notched" loading />
+        <Message.Content>Loading...</Message.Content>
+      </Message>
+    );
+  }
+  if (error) {
+    return (
+      <Message icon negative>
+        <Icon name="attention" />
+        <Message.Content>
+          <Message.Header>Error</Message.Header>
+          {error.message}
+        </Message.Content>
+      </Message>
+    );
+  }
+  return <Input label={me.name} icon={{ name: "send", link: true }} fluid />;
+};
+
+export const SendMessageWithData = graphql(meQuery)(SendMessage);
