718075a2738bde5deb44d436ac09d33c7a2e7803
ianagbip1oti <ianagbip1oti@gmail.com>
2017-10-30 00:44:08 +0000

    Formatting

--- index.jsx
+++ index.jsx
@@ -5,23 +5,29 @@ import { Container, Header, Menu, Segment } from "semantic-ui-react";
 
 import { SendMessageWithData } from "./mat-che/components.jsx";
 
-import { ApolloClient, ApolloProvider, createNetworkInterface } from "react-apollo";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface
+} from "react-apollo";
 
 const networkInterface = createNetworkInterface({
-  uri: '/graphql',
+  uri: "/graphql",
   opts: {
-    credentials: 'same-origin',
-  },
+    credentials: "same-origin"
+  }
 });
 
 const client = new ApolloClient({
-  networkInterface,
+  networkInterface
 });
 
 const App = () => (
   <ApolloProvider client={client}>
     <Container text>
-      <Header as="h1" block>Mat Che</Header>
+      <Header as="h1" block>
+        Mat Che
+      </Header>
       <SendMessageWithData />
     </Container>
   </ApolloProvider>
--- mat-che/resolvers.js
+++ mat-che/resolvers.js
@@ -1,7 +1,7 @@
 import debug from "debug";
 import util from "util";
 
-const log = debug('mat-che:resolvers')
+const log = debug("mat-che:resolvers");
 
 const users = {};
 
@@ -9,17 +9,16 @@ export const resolvers = {
   Query: {
     me: (root, args, ctx) => {
       if (!users[ctx.sid]) return null;
-      
-      return  { name: users[ctx.sid] }
-    }
 
+      return { name: users[ctx.sid] };
+    }
   },
   Mutation: {
     setName: (root, args, ctx) => {
-      log('setting user for: ' + ctx.sid + ' to ' + args["name"]);
+      log("setting user for: " + ctx.sid + " to " + args["name"]);
       users[ctx.sid] = args["name"];
-      
-      return { name: users[ctx.sid] }
+
+      return { name: users[ctx.sid] };
     }
   }
 };
