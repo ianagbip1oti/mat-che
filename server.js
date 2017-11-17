7b7674c941e85a8a411087cf86f0ccf5b8703b13
Jay <11010206+resigned@users.noreply.github.com>
2017-11-16 21:50:45 -0500

    add MemoryStore

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 import session from "express-session";
+import memoryStore from "memorystore";
 
-import { graphqlExpress, graphiqlExpress } from "graphql-server-express";
+import {
+  graphqlExpress,
+  graphiqlExpress
+} from "graphql-server-express";
 
-import { execute, subscribe } from "graphql";
-import { createServer } from "http";
-import { SubscriptionServer } from "subscriptions-transport-ws";
+import {
+  execute,
+  subscribe
+} from "graphql";
+import {
+  createServer
+} from "http";
+import {
+  SubscriptionServer
+} from "subscriptions-transport-ws";
 
 const app = express();
 
@@ -15,10 +26,17 @@ const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
-import { schema } from "./mat-che/schema.js";
+import {
+  schema
+} from "./mat-che/schema.js";
+
+const MemoryStore = memoryStore(session)
 
 app.use(
   session({
+    store: new MemoryStore({
+      checkPeriod: 300 //check for expired entries every 5 minutes
+    }),
     secret: "not-so-secret",
     resave: false,
     saveUninitialized: true
@@ -26,7 +44,9 @@ app.use(
 );
 
 app.get("/", (req, res) =>
-  res.render("index", { google_tracking_id: process.env.GOOGLE_TRACKING_ID })
+  res.render("index", {
+    google_tracking_id: process.env.GOOGLE_TRACKING_ID
+  })
 );
 
 app.get("/bundle.js", (req, res) =>
@@ -38,7 +58,9 @@ app.use(
   bodyParser.json(),
   graphqlExpress(req => ({
     schema: schema,
-    context: { sid: req.session.id }
+    context: {
+      sid: req.session.id
+    }
   }))
 );
 
@@ -53,15 +75,12 @@ app.use(
 const ws = createServer(app);
 
 ws.listen(PORT, () => {
-  new SubscriptionServer(
-    {
-      execute,
-      subscribe,
-      schema
-    },
-    {
-      server: ws,
-      path: "/subscriptions"
-    }
-  );
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema
+  }, {
+    server: ws,
+    path: "/subscriptions"
+  });
 });
